bbc19a772e8b46ded528a296f53de7a89b98d7ae
Minah Kim <kimma1205@gmail.com>
2026-03-10 15:29:26 +0900

    코드 순서변경

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -3,6 +3,10 @@ import { mockPosts } from "../data/mockPosts";
 
 let posts: Post[] = [...mockPosts]
 
+export function createPost(post: Post) {
+    posts.push(post)
+}
+
 export function getPosts() {
     return posts
 }
@@ -11,16 +15,12 @@ export function getPost(id: number) {
     return posts.find(p => p.id ===id)
 }
 
-export function createPost(post: Post) {
-    posts.push(post)
-}
-
-export function deletePost(id: number) {
-    posts = posts.filter(p => p.id !== id)
-}
-
 export function updatePost(updatePost: Post) {
     posts = posts.map(post =>
         post.id === updatePost.id ? updatePost : post
     )
+}
+
+export function deletePost(id: number) {
+    posts = posts.filter(p => p.id !== id)
 }
